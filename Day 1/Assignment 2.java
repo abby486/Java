0435468655bd2bd7538622e9e4bc4072e7f57254
abby486 <61383328+abby486@users.noreply.github.com>
2020-06-10 22:38:32 +0530

    Create Assignment 2.java

---
+++ Day 1/Assignment 2.java
@@ -0,0 +1,44 @@
+//using switch ….case   display whether character is vowel or not.
+
+public class character
+{
+
+	public  static void main(String[] args)
+  {
+	char ch='D';
+	switch(ch)
+{
+	case 'A':
+System.out.println(ch+" "+" Is a Vowel");
+case 'I':
+System.out.println(ch+" "+" Is a Vowel");
+
+case 'O':
+System.out.println(ch+" "+" Is a Vowel");
+break;
+case 'E':
+System.out.println(ch+" "+" Is a Vowel");
+break;
+case 'U':
+System.out.println(ch+" "+" Is a Vowel");
+break;
+case 'a':
+System.out.println(ch+" "+" Is a Vowel");
+break;
+case 'e':
+System.out.println(ch+" "+" Is a Vowel");
+break;
+case 'i':
+System.out.println(ch+" "+" Is a Vowel");
+break;
+case 'o':
+System.out.println(ch+" "+" Is a Vowel");
+break;
+case 'u':
+System.out.println(ch+" "+" Is a Vowel");
+default:
+System.out.println(ch+" "+" Is a Consonant");
+
+	}	
+  }
+}
