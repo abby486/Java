7b9ac47a06844ddd18e6547dce7cda19c3d14354
abby486 <61383328+abby486@users.noreply.github.com>
2020-06-10 22:10:00 +0530

    Create Assignment 1.java

---
+++ Day 1/Assignment 1.java
@@ -0,0 +1,16 @@
+//display a table of a particular number
+
+public class Table
+{
+
+	public  static void main(String[] args)
+  {
+	for(int i=1;i<=10;i++)
+	{
+	int p = 2;
+	int x = p * i;
+	System.out.println(x+"\n");
+
+	}	
+  }
+}
