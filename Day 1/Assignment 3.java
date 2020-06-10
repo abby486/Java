bed000581b28936261c0479b970f619e680a0b21
abby486 <61383328+abby486@users.noreply.github.com>
2020-06-10 22:43:41 +0530

    Create Assignment 3.java

---
+++ Day 1/Assignment 3.java
@@ -0,0 +1,15 @@
+//Display numbers  1 to 10 using do…. While loop.
+
+public class Display
+{
+	public static void main(String[] args)
+	{
+		int a=1;
+		
+		do
+		{
+			System.out.println(a);
+			a++;
+		}while(a<11);
+	}
+}
